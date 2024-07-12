f9ee50906ffb4017b52264d952512fdee485d657
Olanorw <73853649+Olanorw@users.noreply.github.com>
2024-07-12 15:38:31 -0700

    refactor: Update landing page text animation

--- landing_page_text.js
+++ landing_page_text.js
@@ -1,8 +1,16 @@
-// landing_page_text.js
-
 document.addEventListener("DOMContentLoaded", function() {
-    const text = "Hi, I'm Olanorw.";
+    const texts = [
+        "Hi, I'm Olanorw.",
+        "AKA Olav.",
+        "I'm a software developer.",
+        "Welcome to my website.",
+        "Feel free to look around.",
+        "I hope you enjoy your stay.",
+        "Have a great day! :D"
+    ];
     const typingSpeed = 130; // Speed in milliseconds
+    const backspaceSpeed = 80; // Speed for backspacing
+    const pauseBetweenTexts = 1000; // Pause between texts in milliseconds
 
     const landpageContainer = document.getElementById("landpage");
 
@@ -13,20 +21,34 @@ document.addEventListener("DOMContentLoaded", function() {
     landpageContainer.appendChild(newLine);
 
     let i = 0;
+    let textIndex = 0;
     const terminalText = document.getElementById("terminal-text");
     const cursor = document.getElementById("cursor");
 
     function typeWriter() {
-        if (i < text.length) {
-            terminalText.innerHTML = text.slice(0, i) + '<span id="cursor">_</span>';
+        if (i < texts[textIndex].length) {
+            terminalText.innerHTML = texts[textIndex].slice(0, i + 1) + '<span id="cursor">_</span>';
             i++;
             setTimeout(typeWriter, typingSpeed);
         } else {
-            cursor.style.display = 'none'; // Hide cursor after typing is complete
+            if (textIndex < texts.length - 1) {
+                setTimeout(backspace, pauseBetweenTexts); // Start backspacing after a pause
+            } else {
+                cursor.style.display = 'none'; // Hide cursor after all texts are complete
+            }
+        }
+    }
+
+    function backspace() {
+        if (i > 0) {
+            terminalText.innerHTML = texts[textIndex].slice(0, i - 1) + '<span id="cursor">_</span>';
+            i--;
+            setTimeout(backspace, backspaceSpeed);
+        } else {
+            textIndex++;
+            setTimeout(typeWriter, typingSpeed);
         }
     }
 
     setTimeout(typeWriter, 2000); // Start typing after 2 seconds
-
-    set
 });
